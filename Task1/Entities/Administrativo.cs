9d853248d7a6de9b508a18317bdd60f42b5d94f1
JoseAngelDev2 <casuhud1@gmail.com>
2026-06-09 17:34:07 -0400

    Added more classes

---
+++ Task1/Entities/Administrativo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURSO_C_.Entities
+{
+    public class Administrativo : Empleado
+    {
+        public string Departamento {get; set;}
+    }
+}
---
+++ Task1/Entities/ExAlumno.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURSO_C_.Entities
+{
+    public class ExAlumno : MiembroDelaComunidad
+    {
+        public int FechaDeGraduacion {get; set;}
+    }
+}
